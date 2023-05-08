0015f5ee906b02b4d47942e05a49631699bce99e
Radek <rj.airoo@gmail.com>
2023-05-08 13:16:02 +0200

    added funcion edit and delete

--- js/js.js
+++ js/js.js
@@ -33,6 +33,8 @@ const prepareDOMevents = () => {
 	addBtn.addEventListener('click', addNewTodo)
 	ulList.addEventListener('click', checkClick)
 	popupCloseBtn.addEventListener('click', closePopup)
+	popupAddBtn.addEventListener('click', changeTodoText)
+	todoInput.addEventListener('keyup', enterKeyCheck)
 }
 
 const addNewTodo = () => {
@@ -73,18 +75,44 @@ const checkClick = e => {
 		e.target.closest('li').classList.toggle('completed')
 		e.target.classList.toggle('completed')
 	} else if (e.target.matches('.edit')) {
-		editTodo()
+		editTodo(e)
 	} else if (e.target.matches('.delete')) {
-		console.log('delete')
+		deleteTodo(e)
 	}
 }
 
-const editTodo = () => {
+const editTodo = e => {
+	todoToEdit = e.target.closest('li')
+	popupInput.value = todoToEdit.firstChild.textContent
 	popup.style.display = 'flex'
 }
 
 const closePopup = () => {
 	popup.style.display = 'none'
+}
+
+const changeTodoText = () => {
+	if (popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value
+		popup.style.display = 'none'
+		popupInfo.textContent = ''
+	} else {
+		popupInfo.textContent = 'Musisz podać jakąś treść!'
+	}
+}
+
+const deleteTodo = e => {
+	e.target.closest('li').remove()
+	const allTodos = ulList.querySelectorAll('li')
+	if (allTodos.length === 0) {
+		errorInfo.textContent = 'Brak zadań na liście.'
+	}
+}
+
+const enterKeyCheck = e => {
+	if (e.key === 'Enter') {
+		addNewTodo()
+	}
 }
 
 document.addEventListener('DOMContentLoaded', main)
